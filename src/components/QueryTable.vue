e6b124af888f91ed249de18c346d84ebd25c196b
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2021-10-30 18:15:27 -0400

    set up basic table

---
+++ src/components/QueryTable.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="flex flex-col w-full">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div
+          class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
+        >
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  class="
+                    px-6
+                    py-3
+                    text-left text-xs
+                    font-medium
+                    text-gray-500
+                    uppercase
+                    tracking-wider
+                  "
+                >
+                  Query
+                </th>
+                <th scope="col" class="relative px-6 py-3">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <!-- Odd row -->
+              <tr class="bg-white">
+                <td
+                  class="
+                    px-6
+                    py-4
+                    whitespace-nowrap
+                    text-sm
+                    font-medium
+                    text-gray-900
+                  "
+                >
+                  Jane Cooper
+                </td>
+                <td
+                  class="
+                    px-6
+                    py-4
+                    whitespace-nowrap
+                    text-right text-sm
+                    font-medium
+                  "
+                >
+                  <button href="#" class="text-red-600 hover:text-red-900">
+                    Remove
+                  </button>
+                </td>
+              </tr>
+
+              <!-- Even row -->
+              <tr class="bg-gray-50">
+                <td
+                  class="
+                    px-6
+                    py-4
+                    whitespace-nowrap
+                    text-sm
+                    font-medium
+                    text-gray-900
+                  "
+                >
+                  Cody Fisher
+                </td>
+                <td
+                  class="
+                    px-6
+                    py-4
+                    whitespace-nowrap
+                    text-right text-sm
+                    font-medium
+                  "
+                >
+                  <button href="#" class="text-red-600 hover:text-red-900">
+                    Remove
+                  </button>
+                </td>
+              </tr>
+
+              <!-- More people... -->
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'QueryTable',
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,53 @@
         </div>
       </header>
       <main>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8"></div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div>
+            <div class="w-full flex mb-4 mt-4">
+              <label for="query" class="sr-only">Add Query</label>
+              <input
+                type="text"
+                name="query"
+                id="query"
+                class="
+                  shadow-sm
+                  focus:ring-indigo-500 focus:border-indigo-500
+                  block
+                  w-full
+                  sm:text-sm
+                  border-gray-300
+                  rounded-md
+                  mr-4
+                "
+                placeholder="New Query"
+              />
+              <button
+                type="button"
+                class="
+                  inline-flex
+                  items-center
+                  px-4
+                  py-2
+                  border border-transparent
+                  text-sm
+                  font-medium
+                  rounded-md
+                  text-indigo-700
+                  bg-indigo-100
+                  hover:bg-indigo-200
+                  focus:outline-none
+                  focus:ring-2
+                  focus:ring-offset-2
+                  focus:ring-indigo-500
+                "
+              >
+                Add
+              </button>
+            </div>
+          </div>
+
+          <QueryTable />
+        </div>
       </main>
     </MainLayout>
   </div>
@@ -17,11 +63,13 @@
 
 <script>
 import MainLayout from '@/layouts/MainLayout';
+import QueryTable from '@/components/QueryTable';
 
 export default {
   name: 'Home',
   components: {
     MainLayout,
+    QueryTable,
   },
   data() {
     return {};
