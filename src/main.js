1e1e46b424aee4829b1555b79cf19f7dab38772a
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2021-10-30 17:07:05 -0400

    add keycloak

--- src/main.js
+++ src/main.js
@@ -1,11 +1,39 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "windi.css";
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import 'windi.css';
+import router from './router';
+import * as axios from 'axios';
+import VueKeyCloak from '@dsb-norge/vue-keycloak-js';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: (h) => h(App)
-}).$mount("#app");
+function tokenInterceptor() {
+  axios.interceptors.request.use(
+    (config) => {
+      if (Vue.prototype.$keycloak.authenticated) {
+        config.headers.Authorization = `Bearer ${Vue.prototype.$keycloak.token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+}
+
+Vue.use(VueKeyCloak, {
+  config: {
+    realm: 'tigerthon',
+    url: 'http://auth.tigerthon-bot.com/auth',
+    clientId: 'dev',
+  },
+  onReady: () => {
+    tokenInterceptor();
+    /* eslint-disable no-new */
+    new Vue({
+      el: '#app',
+      router,
+      render: (h) => h(App),
+    });
+  },
+});
